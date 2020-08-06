07846189b69fd5cb4ff88ab1004b0f776fda351b
藍奕濡 <a78945612385238@gmail.com>
2020-08-07 03:31:28 +0800

    bug fix

--- router/admin/index.js
+++ router/admin/index.js
@@ -35,8 +35,8 @@ router.post('/login', async (req, res) => {
       .cookie('token', token, {
         httpOnly: true,
         maxAge: 1000 * 60 * 15, // 15min
-        // sameSite: 'none',
-        // secure: true,
+        sameSite: 'none',
+        secure: true,
       })
       .send({ success: true, admin: { email: admin.email, nickname: admin.nickname } });
   } catch (error) {
@@ -55,8 +55,8 @@ router.post('/check', (req, res) => {
       .cookie('token', token, {
         httpOnly: true,
         maxAge: 1000 * 60 * 15, // 15min
-        // sameSite: 'none',
-        // secure: true,
+        sameSite: 'none',
+        secure: true,
       })
       .send({ success: true });
   }
@@ -66,8 +66,8 @@ router.post('/check', (req, res) => {
 // 管理員登出
 router.post('/logout', (req, res) => {
   res.clearCookie('token', {
-    // sameSite: 'none',
-    // secure: true,
+    sameSite: 'none',
+    secure: true,
   });
   return res.send({ success: true, message: '已登出' });
 });
